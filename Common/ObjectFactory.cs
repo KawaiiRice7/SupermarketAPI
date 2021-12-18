6792c5a03c474ffe3ae5c343f383c661b81260f4
KawaiiRice7 <kawaiirice@gmail.com>
2021-12-17 23:33:14 -0800

    Add common directory to repo

---
+++ Common/ObjectFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Unity;
+using System.Collections.Generic;
+
+namespace Common
+{
+    public static class ObjectFactory
+    {
+        private static UnityContainer container = new UnityContainer();
+        public static void RegisterInstance<T>(T obj)
+        {
+            container.RegisterInstance<T>(obj);
+        }
+
+        public static T GetInstance<T>()
+        {
+            return container.Resolve<T>();
+        }
+    }
+}
